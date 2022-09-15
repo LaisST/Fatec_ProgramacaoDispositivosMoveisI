b6c65e339573e7b272143f932537dc1a4f900114
Lais.ST <74365785+LaisST@users.noreply.github.com>
2022-09-15 15:18:36 -0300

    Update Funcionario.kt

--- Atividade 05/Funcionario.kt
+++ Atividade 05/Funcionario.kt
@@ -1,10 +1,36 @@
 fun main() {
 
-   
+    var funcionario = Funcionario()
+
+    println("Nome: ")
+    funcionario.nome = readLine().toString()
+    println("Salario Bruto: ")
+    funcionario.salarioBruto = readLine().toString().toDouble()
+    println("Desconto: ")
+    funcionario.desconto = readLine().toString().toDouble()
+
+    println("Nome: ${funcionario.nome}")
+    println("Salario Liquido: R$ ${funcionario.salarioLiquido()}")
+
+    println("Porcentagem de aumento: ")
+    var porcentagem = readLine().toString().toDouble()
+
+    println("Nome: ${funcionario.nome}")
+    println("Salario Liquido: R$ ${funcionario.salarioLiquido()}")
+    funcionario.aumentarSalario(porcentagem)
+    println("Salario com aumento: R$ ${funcionario.salarioLiquido()}")
+
 
 }
 
-class Funcionario (){
+data class Funcionario(var nome: String = " ", var salarioBruto: Double = 0.0, var desconto: Double = 0.0){
 
+    fun salarioLiquido(): Double{
+        return salarioBruto - desconto
+    }
+
+    fun aumentarSalario(porcentagem: Double){
+        salarioBruto += (porcentagem/100)*salarioBruto
+    }
 
 }
